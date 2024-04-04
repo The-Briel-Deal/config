f9e7385b55a83ae73071ec9435813a02eddecbbf
Gabriel Ford <gabe@fordltc.net>
2024-04-04 00:41:54 -0400

    Commiting changes before merging changes from work

--- nvim/lua/gfconf/remap.lua
+++ nvim/lua/gfconf/remap.lua
@@ -1,10 +1,13 @@
 -- KEYBINDS --
-
 -- Set Leader Key as Spacebar.
 vim.g.mapleader = " "
 
 -- Set Lead+pv as go to Netrw.
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- Set Lead+bp as go to last Buffer.
+vim.keymap.set("n", "<leader>bp", vim.cmd.bp)
+-- Set Lead+bn as go to last Buffer.
+vim.keymap.set("n", "<leader>bn", vim.cmd.bN)
 
 -- Set go Lead+rf as run file.
 vim.keymap.set("n", "<leader>rf", function()
