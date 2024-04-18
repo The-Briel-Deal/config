665b6e7e188d477f328324b1d9d4caeb384e1aff
Gabriel Ford <gabe@fordltc.net>
2024-04-18 20:25:12 +0000

    Add work import to my config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -3,3 +3,7 @@ source ~/.config/shell/alias
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+if test $hostname = "gf.c.googlers.com";
+    source ~/.google_fish_config;
+end;
