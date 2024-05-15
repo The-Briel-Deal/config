c5e92efc3cb8737c28921204af16478fd643ec89
Gabriel Ford <gabriel.ford@mandiant.com>
2024-05-15 16:44:18 -0400

    only source pyenv init on cloudtop, and only eval brewpath on gmac

--- fish/config.fish
+++ fish/config.fish
@@ -4,10 +4,15 @@ source ~/.config/fish/abbr.fish
 # Load My Work Config if I'm on my Work Laptop.
 if test $hostname = "gf.c.googlers.com";
     source ~/.google_fish_config;
+
+    # Setup Pyenv.
+    pyenv init - | source
 end;
 
-# Setup Pyenv.
-pyenv init - | source
+# Eval Brew on my Mac.
+if test $hostname = "gabrielford-macbookpro.roam.internal"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+end
 
 # Set Editor and Theme.
 export EDITOR=vim
