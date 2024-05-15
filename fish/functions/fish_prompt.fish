ec62923c510264d641a558bfaa4d211bb2f744b3
Gabriel Ford <gabe@fordltc.net>
2024-05-15 22:34:28 +0000

    wip shell prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -4,8 +4,26 @@ function fish_prompt
     else
 	set nickname $USER
     end
-    printf '%s%s%s~' \
+
+    switch $hostname
+    case "gf.c.googlers.com"
+	set hostnickname "ctop"
+    case "gabrielford-macbookpro"
+	set hostnickname "gmac"
+    case "*"
+	set hostnickname $hostname
+    end
+    printf '%s%s%s on %s%s%s at %s%s%s%s %s' \
     (set_color $fish_color_user) \
     $nickname \
-    (set_color $fish_color_operator)
+    (set_color $fish_color_operator) \
+    (set_color $fish_color_host) \
+    $hostnickname \
+    (set_color $fish_color_operator) \
+    (set_color $fish_color_status) \
+    (prompt_pwd) \
+    (set_color $fish_color_normal) \
+    (fish_git_prompt) \
+    "=>"
+
 end
