441bb6aed01c775672d1c02e87beed1a867f3023
Gabriel Ford <gabe@fordltc.net>
2024-05-18 01:08:42 -0400

    Clean up nvim config.

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,16 +1,7 @@
 local builtin = require('telescope.builtin')
 local ts_utils = require('telescope.utils')
 
--- Find Files in Vim CWD
-vim.keymap.set('n', '<leader>ff', builtin.find_files)
--- Find Files Not Ignored By Git
-vim.keymap.set('n', '<leader>fg', builtin.git_files)
--- Find Files Matching a Grep String for Contents 
-vim.keymap.set('n', '<leader>fs', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
--- Find Files n Dirs Deep From Curr Buffer
-vim.keymap.set('n', '<leader>fp', function()
+local function find_parent_files()
 	local depth_back = vim.fn.input("Parents Dirs Back >")
 	local path_to_search = ts_utils.buffer_dir()
 	for _ = 1, depth_back, 1 do
@@ -20,11 +11,34 @@ vim.keymap.set('n', '<leader>fp', function()
 	print(path_to_search)
 	builtin.find_files({ cwd = path_to_search })
 end
-)
--- Find Files in Home
-vim.keymap.set('n', '<leader>fh', function()
-	builtin.find_files({ cwd = '~', hidden=true })
-end
-)
+
+-- Find Files in Vim CWD
+vim.keymap.set('n', '<leader>ff', builtin.find_files)
+
+-- Find Files Not Ignored By Git
+vim.keymap.set('n', '<leader>fg', builtin.git_files)
+
+-- Find Files Matching a Grep String for Contents
+vim.keymap.set('n', '<leader>fs', function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
+
+-- Find Files n Dirs Deep From Curr Buffer
+vim.keymap.set('n', '<leader>fp', find_parent_files)
+
+-- Find Help Docs
+vim.keymap.set('n', '<leader>fh', builtin.help_tags)
+
+-- Find Files in My Config
+vim.keymap.set('n', '<leader>fc', function()
+	builtin.find_files({ cwd = '~/.config' })
+end)
+
+-- Search for a line in curr buffer
+vim.keymap.set('n', '<leader>f/', builtin.current_buffer_fuzzy_find)
+
+-- Choose one of my favorite Directories
+vim.keymap.set('n', '<leader>fo', builtin.oldfiles)
+
 -- Telescope Buffers
 vim.keymap.set('n', '<leader>fb', builtin.buffers)
--- nvim/lua/gfconf/lazy.lua
+++ nvim/lua/gfconf/lazy.lua
@@ -68,5 +68,17 @@ require("lazy").setup({
 	},
 	{
 		"ndonfris/fish-lsp"
+	},
+	{
+		"vhyrro/luarocks.nvim",
+		priority = 1000,
+		config = true,
+	},
+	{
+		"nvim-neorg/neorg",
+		dependencies = { "luarocks.nvim" },
+		lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+		version = "*", -- Pin Neorg to the latest stable release
+		config = true,
 	}
 })
