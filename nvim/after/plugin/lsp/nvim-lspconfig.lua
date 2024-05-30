92bc4210439fd4c403c55086a36b0f599e6e550c
Gabriel Ford <gabe@fordltc.net>
2024-05-30 02:47:15 +0000

    Fix nvim warning.

--- nvim/after/plugin/lsp/nvim-lspconfig.lua
+++ nvim/after/plugin/lsp/nvim-lspconfig.lua
@@ -1,4 +1,5 @@
 local lspconfig = require('lspconfig')
+local configs = require("lspconfig.configs")
 local neodev = require('neodev')
 local cmp = require('cmp')
 local lsp_zero = require('lsp-zero')
@@ -78,3 +79,19 @@ require('mason-lspconfig').setup({
 		end,
 	},
 })
+
+
+if vim.fn.hostname() == "gf.c.googlers.com" then
+	configs.ciderlsp = {
+		default_config = {
+			cmd = { "/google/bin/releases/cider/ciderlsp/ciderlsp", "--tooltag=nvim-cmp", "--noforward_sync_responses" },
+			filetypes = { "c", "cpp", "java", "kotlin", "objc", "proto", "textproto", "go", "python", "bzl" },
+			root_dir = lspconfig.util.root_pattern("BUILD"),
+			settings = {},
+		},
+	}
+
+	lspconfig.ciderlsp.setup({
+		capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
+	})
+end
--- nvim/lua/gfconf/lazy.lua
+++ nvim/lua/gfconf/lazy.lua
@@ -111,14 +111,6 @@ require("lazy").setup({
 		priority = 1000,
 		config = true,
 	},
-	-- Org mode for Neovim
-	{
-		"nvim-neorg/neorg",
-		dependencies = { "luarocks.nvim" },
-		lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
-		version = "*", -- Pin Neorg to the latest stable release
-		config = true,
-	},
 	-- A bunch of miscellaneous mini plugins
 	{
 		'echasnovski/mini.nvim',
