a21ee99d33fa21f6724ee1a4aff34c2c556af3db
Gabriel Ford <gabe@fordltc.net>
2024-07-12 21:50:23 -0400

    Added step over and terminate binds.

--- nvim/after/plugin/dap/nvim-dap.lua
+++ nvim/after/plugin/dap/nvim-dap.lua
@@ -1,16 +1,25 @@
 local dap = require('dap')
 
--- No more stinky UI.
--- ui.setup()
--- dap.listeners.after.event_initialized["dapui_config"] = function()
--- 	ui.open()
--- end
--- dap.listeners.before.event_terminated["dapui_config"] = function()
--- 	ui.close()
--- end
--- dap.listeners.before.event_exited["dapui_config"] = function()
--- 	ui.close()
--- end
+dap.adapters.gdb = {
+	type = "executable",
+	command = "gdb",
+	args = { "-i", "dap" }
+}
 
-vim.keymap.set('n', '<Leader>db', dap.set_breakpoint)
+dap.configurations.cpp = {
+	{
+		name = "Launch",
+		type = "gdb",
+		request = "launch",
+		program = function()
+			return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+		end,
+		cwd = "${workspaceFolder}",
+		stopAtBeginningOfMainSubprogram = false,
+	}
+}
+
+vim.keymap.set('n', '<Leader>db', dap.toggle_breakpoint)
 vim.keymap.set('n', '<Leader>dr', dap.continue)
+vim.keymap.set('n', '<Leader>dn', dap.step_over)
+vim.keymap.set('n', '<Leader>dc', dap.terminate)
