2f5e6900d8e29a556a99021d864a6d43ab21e856
Gabriel Ford <gabe@fordltc.net>
2024-07-15 13:25:42 +0000

    Purge unused nvim plugins.

--- nvim/after/plugin/dap/nvim-dap.lua
+++ nvim/after/plugin/dap/nvim-dap.lua
@@ -1,16 +1,16 @@
 local dap = require('dap')
-local ui = require('dapui')
 
-ui.setup()
-dap.listeners.after.event_initialized["dapui_config"] = function()
-	ui.open()
-end
-dap.listeners.before.event_terminated["dapui_config"] = function()
-	ui.close()
-end
-dap.listeners.before.event_exited["dapui_config"] = function()
-	ui.close()
-end
+-- No more stinky UI.
+-- ui.setup()
+-- dap.listeners.after.event_initialized["dapui_config"] = function()
+-- 	ui.open()
+-- end
+-- dap.listeners.before.event_terminated["dapui_config"] = function()
+-- 	ui.close()
+-- end
+-- dap.listeners.before.event_exited["dapui_config"] = function()
+-- 	ui.close()
+-- end
 
 vim.keymap.set('n', '<Leader>db', dap.set_breakpoint)
 vim.keymap.set('n', '<Leader>dr', dap.continue)
