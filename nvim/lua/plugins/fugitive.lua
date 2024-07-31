540fcd7e93e65faca47acb508347d7a29ce4d0f7
Gabriel Ford <gabe@fordltc.net>
2024-07-30 23:46:39 -0400

    Setup lazy loading for fugitive.

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -1,5 +1,11 @@
 -- Sweet Succulent Git Plugin for Vim 🤤
+---@type LazySpec
 return {
 	"tpope/vim-fugitive",
-	as = "fugitive"
+	as = "fugitive",
+	cmd = "Git",
+	keys = {
+		{ "<leader>gs", vim.cmd.Git,                         mode = "n", desc = "Git Status" },
+		{ "<leader>gb", function() vim.cmd.Git("blame") end, mode = "n", desc = "Git Blame" }
+	}
 }
