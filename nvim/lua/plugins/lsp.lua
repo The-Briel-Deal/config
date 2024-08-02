c17bdfc9c184849dacfff195ce3be12c098e9531
Gabriel Ford <gabe@fordltc.net>
2024-08-02 21:10:34 +0000

    Add a check for if the ciderlsp dir exists.

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,4 +1,6 @@
 -- Install nvim-cmp and other lsp things!
+local CIDER_LSP_DIR = '/google/bin/releases/cider/ciderlsp/ciderlsp'
+
 ---@type LazySpec
 return {
 	"neovim/nvim-lspconfig",
@@ -6,16 +8,19 @@ return {
 		local nvim_lspconfig = require("lspconfig")
 		local lsp_configs = require("lspconfig.configs")
 
-		lsp_configs.ciderlsp = {
-			default_config = {
-				cmd = { '/google/bin/releases/cider/ciderlsp/ciderlsp', '--tooltag=nvim-lsp', '--noforward_sync_responses' },
-				filetypes = { "c", "cpp", "java", "kotlin", "objc", "proto", "textproto", "go", "python", "bzl", "typescript" },
-				offset_encoding = 'utf-8',
-				root_dir = nvim_lspconfig.util.root_pattern('google3/*BUILD'),
-				settings = {},
+		-- Setup CiderLSP if i'm its path exists.
+		if (vim.fn.isdirectory(CIDER_LSP_DIR)) then
+			lsp_configs.ciderlsp = {
+				default_config = {
+					cmd = { CIDER_LSP_DIR, '--tooltag=nvim-lsp', '--noforward_sync_responses' },
+					filetypes = { "c", "cpp", "java", "kotlin", "objc", "proto", "textproto", "go", "python", "bzl", "typescript" },
+					offset_encoding = 'utf-8',
+					root_dir = nvim_lspconfig.util.root_pattern('google3/*BUILD'),
+					settings = {},
+				}
 			}
-		}
-		nvim_lspconfig.ciderlsp.setup({})
+			nvim_lspconfig.ciderlsp.setup({})
+		end
 	end,
 	keys = {
 		{ "gd", vim.lsp.buf.definition, desc = "Goto Definition" },
