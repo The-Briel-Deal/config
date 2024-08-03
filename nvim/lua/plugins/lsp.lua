c7b6965429e82df2a8b48c85d6671b41432a5ac2
Gabriel Ford <gabe@fordltc.net>
2024-08-03 13:52:09 -0400

    Move harpoon binds to lazy.

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -16,18 +16,12 @@ return {
 							vim.snippet.expand(args.body)
 						end,
 					},
-					window = {
-					--[[
-						completion = require('cmp').config.window.bordered(),
-						documentation = require('cmp').config.window.bordered(),
-					]]
-					},
 					mapping = require('cmp').mapping.preset.insert({
 						['<C-b>'] = require('cmp').mapping.scroll_docs(-4),
 						['<C-f>'] = require('cmp').mapping.scroll_docs(4),
 						['<C-Space>'] = require('cmp').mapping.complete(),
 						['<C-e>'] = require('cmp').mapping.abort(),
-						['<CR>'] = require('cmp').mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+						['<CR>'] = require('cmp').mapping.confirm({ select = true }),
 					}),
 					sources = require('cmp').config.sources({
 						{ name = 'nvim_lsp' },
