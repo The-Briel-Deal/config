12573dfee15d87f46902986b46ae5d812cf92082
Gabriel Ford <gabe@fordltc.net>
2024-08-04 14:45:03 -0400

    Cleanup keymaps and move zen-mode map to lazy plugin.

--- nvim/lua/config/keymap.lua
+++ nvim/lua/config/keymap.lua
@@ -2,57 +2,50 @@
 -- Set Leader Key as Spacebar.
 vim.g.mapleader = " "
 
+local set = vim.keymap.set
+
 -- Set Lead+pv as go to Netrw.
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+set("n", "<leader>pv", vim.cmd.Ex)
 -- Set Lead+bp as go to last Buffer.
-vim.keymap.set("n", "<leader>bp", vim.cmd.bp)
+set("n", "<leader>bp", vim.cmd.bp)
 -- Set Lead+bn as go to last Buffer.
-vim.keymap.set("n", "<leader>bn", vim.cmd.bN)
+set("n", "<leader>bn", vim.cmd.bN)
 -- Set lead-s to quick-save.
-vim.keymap.set("n", "<leader>s", vim.cmd.w)
+set("n", "<leader>s", vim.cmd.w)
 -- Set lead-S to quick-save and close window.
-vim.keymap.set("n", "<leader>S", vim.cmd.wq)
+set("n", "<leader>S", vim.cmd.wq)
 -- Set lead-gh as the command to go home in netrw.
-vim.keymap.set("n", "<leader>gh", function() vim.cmd("Vexplore ~/") end)
+set("n", "<leader>gh", function() vim.cmd("Vexplore ~/") end)
 -- Set lead-c as the command to close current split.
-vim.keymap.set("n", "<leader>c", vim.cmd.close)
+set("n", "<leader>c", vim.cmd.close)
 -- Use lead to move between windows.
-vim.keymap.set("n", "<leader>h", function() vim.cmd.wincmd("h") end)
-vim.keymap.set("n", "<leader>j", function() vim.cmd.wincmd("j") end)
-vim.keymap.set("n", "<leader>k", function() vim.cmd.wincmd("k") end)
-vim.keymap.set("n", "<leader>l", function() vim.cmd.wincmd("l") end)
+set("n", "<leader>h", function() vim.cmd.wincmd("h") end)
+set("n", "<leader>j", function() vim.cmd.wincmd("j") end)
+set("n", "<leader>k", function() vim.cmd.wincmd("k") end)
+set("n", "<leader>l", function() vim.cmd.wincmd("l") end)
 
 -- New tab page.
-vim.keymap.set("n", "<leader>tn", vim.cmd.tabnew)
+set("n", "<leader>tn", vim.cmd.tabnew)
 -- Close tab page.
-vim.keymap.set("n", "<leader>tc", vim.cmd.tabclose)
+set("n", "<leader>tc", vim.cmd.tabclose)
 -- New tab term.
-vim.keymap.set("n", "<leader>tt", '<cmd>tab term<cr>')
+set("n", "<leader>tt", '<cmd>tab term<cr>')
 -- New tab explorer.
-vim.keymap.set("n", "<leader>te", '<cmd>Texplore<cr>')
+set("n", "<leader>te", '<cmd>Texplore<cr>')
 -- Next tab.
-vim.keymap.set("n", "<leader>tl", '<cmd>tabnext<cr>')
+set("n", "<leader>tl", '<cmd>tabnext<cr>')
 -- Last tab.
-vim.keymap.set("n", "<leader>th", '<cmd>tabprevious<cr>')
+set("n", "<leader>th", '<cmd>tabprevious<cr>')
 
 -- Explore Home.
-vim.keymap.set("n", "<leader>eh", '<cmd>Explore ~<cr>')
+set("n", "<leader>eh", '<cmd>Explore ~<cr>')
 
 -- Open new Daily Note.
-vim.keymap.set("n", "<leader>nd", function()
+set("n", "<leader>nd", function()
 	local template_path = "~/Notes/Templates/nvim-daily-note.md"
 	local daily_note_path = '~/Notes/Daily/' .. vim.fn.strftime('%Y-%m-%d') .. '.md'
 	if (vim.fn.empty(vim.fn.glob(vim.fn.expand(daily_note_path))) == 1) then
 		os.execute("cp " .. template_path .. " " .. daily_note_path)
 	end
 	vim.cmd.edit(daily_note_path)
-end)
-
--- ZenMode Hotkey
-vim.keymap.set("n", "<leader>zm", function()
-	require("zen-mode").toggle({
-		window = {
-			width = .75
-		}
-	})
 end)
--- nvim/lua/plugins/zen-mode.lua
+++ nvim/lua/plugins/zen-mode.lua
@@ -1,4 +1,7 @@
 -- Zen Mode is a plugin to fullscreen one buffer to remove distractions
 return {
-	"folke/zen-mode.nvim"
+	"folke/zen-mode.nvim",
+	keys = {
+		{ "<leader>zm", function() require("zen-mode").toggle({ window = { width = .75 } }) end },
+	},
 }
