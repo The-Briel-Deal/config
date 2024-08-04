d53a4a0abe3bb17c6c4319e6f3881ab29eade293
Gabriel Ford <gabe@fordltc.net>
2024-08-03 20:41:16 -0400

    Moved Everything to Lazy.nvim

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,29 @@
 -- Install Treesitter for Syntax Highlighting
 return {
 	'nvim-treesitter/nvim-treesitter',
-	run = ':TSUpdate'
+	event = "BufEnter",
+	run = ':TSUpdate',
+	opt = {
+		-- A list of parser names, or "all" (the five listed parsers should always be installed)
+		ensure_installed = { "python", "javascript", "typescript", "cpp", "c", "lua", "vim", "vimdoc", "query", "bash" },
+
+		-- Install parsers synchronously (only applied to `ensure_installed`)
+		sync_install = true,
+
+		-- Automatically install missing parsers when entering buffer, uses TS CLI.
+		auto_install = true,
+
+		-- Enable Syntax Highlighting Based on Treesitter.
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = false,
+		},
+
+		-- Enable indentation.
+		indent = {
+			enable = true
+		}
+	}
+
+
 }
--- nvim/lua/plugins/undotree.lua
+++ nvim/lua/plugins/undotree.lua
@@ -1,4 +1,8 @@
 -- Undo Tree for going back through undos and redos
 return {
-	"mbbill/undotree"
+	"mbbill/undotree",
+	event = "BufEnter",
+	keys = {
+		{ "<leader>u", vim.cmd.UndotreeToggle, desc = "Toggle UndoTree" },
+	}
 }
