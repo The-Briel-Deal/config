742353517685266cd3dc6043c9bf7de8e98d1e19
Gabriel Ford <gabe@fordltc.net>
2024-10-10 20:07:59 +0000

    Changed harpoon keys to use ctrl instead of alt

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -7,10 +7,10 @@ return {
 	keys = {
 		{ "<leader>a", function() require('harpoon'):list():add() end,                                    desc = "Add To Harpoon List" },
 		{ "<C-e>",     function() require('harpoon').ui:toggle_quick_menu(require('harpoon'):list()) end, desc = "Open Harpoon List" },
-		{ "<A-a>",     function() require('harpoon'):list():select(1) end,                                desc = "Go to Mark 1" },
-		{ "<A-s>",     function() require('harpoon'):list():select(2) end,                                desc = "Go to Mark 2" },
-		{ "<A-d>",     function() require('harpoon'):list():select(3) end,                                desc = "Go to Mark 3" },
-		{ "<A-f>",     function() require('harpoon'):list():select(4) end,                                desc = "Go to Mark 4" },
-		{ "<A-g>",     function() require('harpoon'):list():select(4) end,                                desc = "Go to Mark 4" },
+		{ "<C-a>",     function() require('harpoon'):list():select(1) end,                                desc = "Go to Mark 1" },
+		{ "<C-s>",     function() require('harpoon'):list():select(2) end,                                desc = "Go to Mark 2" },
+		{ "<C-d>",     function() require('harpoon'):list():select(3) end,                                desc = "Go to Mark 3" },
+		{ "<C-f>",     function() require('harpoon'):list():select(4) end,                                desc = "Go to Mark 4" },
+		{ "<C-g>",     function() require('harpoon'):list():select(5) end,                                desc = "Go to Mark 4" },
 	}
 }
