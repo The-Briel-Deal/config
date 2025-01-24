06b7e93141be37b5509f05375cae0c783b2053de
gabe <gabe@fordltc.net>
2025-01-23 23:21:56 -0500

    Add formatting bind.

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -1,20 +1,27 @@
 vim.api.nvim_create_autocmd('LspAttach', {
 	callback = function(_)
-    local function on_list(options)
-      vim.fn.setqflist({}, ' ', options)
-      vim.cmd.cfirst()
-    end
-    
-    local set = vim.keymap.set
-    
-    set('n', 'gd', function()
-      vim.lsp.buf.definition({ on_list = on_list})
-    end, {buffer = true})
-    
-    set('n', 'gr', function()
-      vim.lsp.buf.references({includeDeclaration = false}, { on_list = on_list})
-    end, {buffer = true, nowait = true})
+		local function on_list(options)
+			vim.fn.setqflist({}, ' ', options)
+			vim.cmd.cfirst()
+		end
 
+		local set = vim.keymap.set
+
+		-- Go to Definition
+		set('n', 'gd', function()
+			vim.lsp.buf.definition({ on_list = on_list })
+		end, { buffer = true })
+
+		-- Go to References
+		set('n', 'gr', function()
+			vim.lsp.buf.references({ includeDeclaration = false }, { on_list = on_list })
+		end, { buffer = true, nowait = true })
+
+		-- Format
+		set('n', '<F3>', function()
+			vim.lsp.buf.format()
+		end, { buffer = true })
+
+		vim.api.nvim_create_user_command("Format", function(_) vim.lsp.buf.format() end, {})
 	end
 })
-
