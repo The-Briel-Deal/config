b0f60a6208cba0d6f4c1a780f301df3476216477
gabe <gabe@fordltc.net>
2025-01-23 23:00:31 -0500

    Put my LSP binds in an Autocommand that is only run on attach.

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,14 +1,22 @@
-local function on_list(options)
-  vim.fn.setqflist({}, ' ', options)
-  vim.cmd.cfirst()
-end
 
-local set = vim.keymap.set
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(_ev)
 
-set('n', 'gd', function()
-  vim.lsp.buf.definition({ on_list = on_list})
-end, {buffer = true})
+    local function on_list(options)
+      vim.fn.setqflist({}, ' ', options)
+      vim.cmd.cfirst()
+    end
+    
+    local set = vim.keymap.set
+    
+    set('n', 'gd', function()
+      vim.lsp.buf.definition({ on_list = on_list})
+    end, {buffer = true})
+    
+    set('n', 'gr', function()
+      vim.lsp.buf.references({includeDeclaration = false}, { on_list = on_list})
+    end, {buffer = true, nowait = true})
 
-set('n', 'gr', function()
-  vim.lsp.buf.references({includeDeclaration = false}, { on_list = on_list})
-end, {buffer = true, nowait = true})
+	end
+})
+
