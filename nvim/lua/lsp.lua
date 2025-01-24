f210ba4b056a8ff0dd7ccff7a1bb1364a5cc18fa
gabe <gabe@fordltc.net>
2025-01-23 22:49:14 -0500

    Setup config for LSP.

---
+++ nvim/lua/lsp.lua
@@ -0,0 +1,14 @@
+local function on_list(options)
+  vim.fn.setqflist({}, ' ', options)
+  vim.cmd.cfirst()
+end
+
+local set = vim.keymap.set
+
+set('n', 'gd', function()
+  vim.lsp.buf.definition({ on_list = on_list})
+end, {buffer = true})
+
+set('n', 'gr', function()
+  vim.lsp.buf.references({includeDeclaration = false}, { on_list = on_list})
+end, {buffer = true, nowait = true})
