40effd18769844b94d58755d8190b11b0606b91e
gabe <gabe@fordltc.net>
2025-01-25 11:32:02 -0500

    Make * use wl-copy

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -1,33 +1,33 @@
 local M = {}
 
 M.setup = function(opts)
-  vim.cmd[[colorscheme tokyonight]]
+	vim.cmd [[colorscheme tokyonight]]
 
-  vim.go.number = true
-  vim.go.relativenumber = true
-  vim.go.termsync = false
-  vim.go.tabstop = 2
-  vim.go.shiftwidth = 2
-  vim.g.netrw_banner = false
-  vim.g.clipboard = {
-    name = "Tmux Clipboard",
-    copy = {
-      ["+"] = {'tmux', 'load-buffer', '-'},
-      ["*"] = {'tmux', 'load-buffer', '-'},
-    },
-    paste = {
-      ["+"] = {'tmux', 'save-buffer', '-'},
-      ["*"] = {'tmux', 'save-buffer', '-'},
-    },
-  }
-  
-  -- Highlight on Yank
-  vim.api.nvim_create_autocmd({ 'TextYankPost' }, {
-    pattern = "*",
-    callback = function()
-      vim.hl.on_yank { higroup = 'IncSearch', timeout = 130 }
-    end
-  })
+	vim.go.number = true
+	vim.go.relativenumber = true
+	vim.go.termsync = false
+	vim.go.tabstop = 2
+	vim.go.shiftwidth = 2
+	vim.g.netrw_banner = false
+	vim.g.clipboard = {
+		name = "Tmux Clipboard",
+		copy = {
+			["+"] = { 'tmux', 'load-buffer', '-' },
+			["*"] = { 'wl-copy' },
+		},
+		paste = {
+			["+"] = { 'tmux', 'save-buffer', '-' },
+			["*"] = { 'wl-paste' },
+		},
+	}
+
+	-- Highlight on Yank
+	vim.api.nvim_create_autocmd({ 'TextYankPost' }, {
+		pattern = "*",
+		callback = function()
+			vim.hl.on_yank { higroup = 'IncSearch', timeout = 130 }
+		end
+	})
 end
 
 return M
