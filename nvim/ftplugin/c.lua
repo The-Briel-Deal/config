915ee2a2178207eb4f8e251727624e857bda4e09
Gabriel Ford <gabe@fordltc.net>
2025-01-25 19:43:44 -0500

    Add ftplugin for C.

---
+++ nvim/ftplugin/c.lua
@@ -0,0 +1,7 @@
+vim.lsp.start({
+	name = 'clangd',
+	cmd = { 'clangd' },
+	root_dir = vim.fs.dirname(vim.fs.find({ "Makefile", "compile_commands.json", ".gitignore" }, { upward = true })[1]),
+	settings = {
+	}
+})
