2729866b3e8da2d1af3ba03d91e133a330e0f7ca
Gabriel Ford <gabe@fordltc.net>
2025-01-31 21:59:40 -0500

    Change breakpoint bind to <lead>bb.
    So I don't keep triggering my tmux prefix.

--- nvim/plugin/gdb.lua
+++ nvim/plugin/gdb.lua
@@ -1,5 +1,5 @@
 local set = vim.keymap.set
-set('n', '<leader>db', function()
+set('n', '<leader>bb', function()
 	local filename = vim.fn.expand("%:t")
 	local line_num = vim.fn.line('.')
 	local break_stmt = string.format("break %s:%i\n", filename, line_num)
