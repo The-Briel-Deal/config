e98df73548ea235dc18b8a80428efd483ff4fa88
Gabriel Ford <gabe@fordltc.net>
2025-02-01 19:51:37 -0500

    Add makefiles to list of treesitter.start autocmds

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd('FileType', {
-	pattern = {'markdown', 'c', 'python'},
+	pattern = { 'markdown', 'c', 'python', 'make' },
 	callback = function(args)
 		vim.treesitter.start(args.buf)
 	end
