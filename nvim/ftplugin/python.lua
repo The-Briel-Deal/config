6b2f24c6dbd22e619765cbf4d30e1b9f229f88c6
Gabriel Ford <gabe@fordltc.net>
2025-02-18 18:16:56 +0000

    Add pyright lsp support.

--- nvim/ftplugin/python.lua
+++ nvim/ftplugin/python.lua
@@ -1,2 +1,69 @@
-vim.opt_global.python_recommended_style = 0
+-- vim.opt_global.python_recommended_style = 0
 vim.opt_local.shiftwidth = 2
+
+local root_files = {
+	'pyproject.toml',
+	'setup.py',
+	'setup.cfg',
+	'requirements.txt',
+	'Pipfile',
+	'pyrightconfig.json',
+	'.git',
+}
+
+--local function organize_imports()
+--	local params = {
+--		command = 'pyright.organizeimports',
+--		arguments = { vim.uri_from_bufnr(0) },
+--	}
+--
+--	local clients = util.get_lsp_clients {
+--		bufnr = vim.api.nvim_get_current_buf(),
+--		name = 'pyright',
+--	}
+--	for _, client in ipairs(clients) do
+--		client.request('workspace/executeCommand', params, nil, 0)
+--	end
+--end
+--
+--local function set_python_path(path)
+--	local clients = util.get_lsp_clients {
+--		bufnr = vim.api.nvim_get_current_buf(),
+--		name = 'pyright',
+--	}
+--	for _, client in ipairs(clients) do
+--		if client.settings then
+--			client.settings.python = vim.tbl_deep_extend('force', client.settings.python, { pythonPath = path })
+--		else
+--			client.config.settings = vim.tbl_deep_extend('force', client.config.settings, { python = { pythonPath = path } })
+--		end
+--		client.notify('workspace/didChangeConfiguration', { settings = nil })
+--	end
+--end
+
+vim.lsp.start({
+	name = 'pyright',
+	cmd = { 'pyright-langserver', '--stdio' },
+	root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
+	settings = {
+		python = {
+			analysis = {
+				autoSearchPaths = true,
+				useLibraryCodeForTypes = true,
+				diagnosticMode = 'openFilesOnly',
+			},
+		},
+	},
+	--	commands = {
+	--		PyrightOrganizeImports = {
+	--			organize_imports,
+	--			description = 'Organize Imports',
+	--		},
+	--		PyrightSetPythonPath = {
+	--			set_python_path,
+	--			description = 'Reconfigure pyright with the provided python path',
+	--			nargs = 1,
+	--			complete = 'file',
+	--		},
+	--	}
+})
