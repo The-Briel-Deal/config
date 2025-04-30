37ae3ee423decc57778d7fb0bc8953129c431d20
Gabriel Ford <gabe@fordltc.net>
2025-04-30 15:24:36 +0000

    Format cl selection command and save result in env var.

--- misc_scripts/go_to_cl.sh
+++ misc_scripts/go_to_cl.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-gob-curl -s 'https://gso-fact-internal-review.git.corp.google.com/changes/?q=owner:gabrielford@google.com+is:open' | tail -n +2 | jq -r '.[] | "\(.subject) -- \(._number) -- \(.current_revision_number)"' | fzf | lolcat
+SELECTED=$(gob-curl -s 'https://gso-fact-internal-review.git.corp.google.com/changes/?q=owner:gabrielford@google.com+is:open' \
+  | tail -n +2 \
+	| jq -r '.[] | "\(.subject) -- \(._number) -- \(.current_revision_number)"' \
+  | fzf)
+
+echo $SELECTED
