6f4c05c2d00d8863a2fb5053f6c822c13bbd34cf
Gabriel Ford <gabe@fordltc.net>
2025-04-30 15:44:28 +0000

    Pull out cl number and number of revisions.

--- misc_scripts/go_to_cl.sh
+++ misc_scripts/go_to_cl.sh
@@ -5,4 +5,11 @@ SELECTED=$(gob-curl -s 'https://gso-fact-internal-review.git.corp.google.com/cha
 	| jq -r '.[] | "\(.subject) -- \(._number) -- \(.current_revision_number)"' \
   | fzf)
 
-echo $SELECTED
+IFS=$'\n'
+
+declare -a 'SPLIT=($(sed "s/ -- /\n/g" <<< $SELECTED))'
+
+CL_NUMBER=${SPLIT[1]}
+CL_LATEST_REVISION=${SPLIT[2]}
+
+echo $CL_NUMBER has $CL_LATEST_REVISION revisions
