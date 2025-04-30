d32ac3846cf9478912c674434ba77cd407f239c8
Gabriel Ford <gabe@fordltc.net>
2025-04-30 16:02:48 +0000

    Include repo name in output message.

--- misc_scripts/go_to_cl.sh
+++ misc_scripts/go_to_cl.sh
@@ -9,10 +9,11 @@ IFS=$'\n'
 
 declare -a 'SPLIT=($(sed "s/ -- /\n/g" <<< $SELECTED))'
 
+REPO_NAME=${SPLIT[0]}
 CL_NUMBER=${SPLIT[2]}
 CL_LATEST_REVISION=${SPLIT[3]}
 
-echo "Pulling CL $CL_NUMBER at revision $CL_LATEST_REVISION."
+echo "Fetching $REPO_NAME CL $CL_NUMBER at revision $CL_LATEST_REVISION."
 
 CL_REMOTE_REF=refs/changes/${CL_NUMBER:(-2)}/${CL_NUMBER}/${CL_LATEST_REVISION}
 
