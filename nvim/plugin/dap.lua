b403d278bf8a6ff89482d3833d69c76a6c85fffb
Gabriel Ford <gabe@fordltc.net>
2025-05-31 13:19:11 -0400

    remove old and unused configs

--- nvim/plugin/dap.lua
+++ nvim/plugin/dap.lua
@@ -12,23 +12,13 @@ dap.adapters.gdb = {
 	args = { '--interpreter=dap', '--eval-command', 'set print pretty on' }
 }
 
-local lldb_adapter = {
+dap.adapters.lldb = {
 	type = 'executable',
 	command = '/usr/bin/lldb-dap',
 	name = 'lldb',
 	args = {},
 }
-dap.adapters.lldb = lldb_adapter
 
-local launch_nvim_headless_conf = {
-	name = 'Launch nvim headless',
-	type = 'lldb',
-	request = 'launch',
-	program = function()
-		return vim.fn.getcwd() .. '/build/bin/nvim'
-	end,
-	args = { '--headless', '--listen', 'localhost:12345' }
-}
 
 dap.configurations.c = {
 	{
@@ -39,65 +29,9 @@ dap.configurations.c = {
 			return vim.fn.input('Path to executable:', vim.fn.getcwd() .. '/', 'file')
 		end,
 		runInTerminal = true,
-	},
-	launch_nvim_headless_conf,
+	}
 }
 
-dap.adapters.python = function(cb, config)
-	if config.request == 'attach' then
-		---@diagnostic disable-next-line: undefined-field
-		local port = (config.connect or config).port
-		---@diagnostic disable-next-line: undefined-field
-		local host = (config.connect or config).host or '127.0.0.1'
-		cb({
-			type = 'server',
-			port = assert(port, '`connect.port` is required for a python `attach` configuration'),
-			host = host,
-			options = {
-				source_filetype = 'python',
-			},
-		})
-	else
-		cb({
-			type = 'executable',
-			command = 'python',
-			args = { '-m', 'debugpy.adapter' },
-			options = {
-				source_filetype = 'python',
-			},
-		})
-	end
-end
-
-dap.configurations.python = {
-	{
-		-- The first three options are required by nvim-dap
-		type = 'python', -- the type here established the link to the adapter definition: `dap.adapters.python`
-		request = 'launch',
-		name = "Launch file",
-
-		-- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
-
-		program = "psteal", -- This configuration will launch the current file if used.
-		pythonPath = function()
-			-- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
-			-- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
-			-- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
-			local cwd = vim.fn.getcwd()
-			if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
-				return cwd .. '/venv/bin/python'
-			elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
-				return cwd .. '/.venv/bin/python'
-			else
-				return '/usr/bin/python'
-			end
-		end,
-	},
-}
-
-local dap_widgets = require('dap.ui.widgets')
-
-local set = vim.keymap.set
 
 local function session_active()
 	if dap.session() then
@@ -107,11 +41,8 @@ local function session_active()
 	return false
 end
 
-set('n', '<leader>drn', function()
-	dap.run(launch_nvim_headless_conf)
-	local suc, exitcode, code = os.execute('tmux splitw -b nvim --remote-ui --server localhost:12345')
-	print("suc: " .. tostring(suc) .. " exitcode: " .. tostring(exitcode) .. " code: " .. tostring(code))
-end)
+local dap_widgets = require('dap.ui.widgets')
+local set = vim.keymap.set
 
 set('n', '<leader>dc', function()
 	dap.continue()
