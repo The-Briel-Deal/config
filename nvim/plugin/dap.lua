2e03f799b55b04ab908642fa7c648b91e72964b0
Gabriel Ford <gabe@fordltc.net>
2025-06-01 16:56:18 -0400

    get running both processes at once working

--- nvim/plugin/dap.lua
+++ nvim/plugin/dap.lua
@@ -5,54 +5,90 @@ dap.defaults.fallback.external_terminal = {
 	args = { 'splitw' }
 }
 
-dap.adapters.gdb = {
-	type = 'executable',
-	command = 'gdb',
-	args = { '--interpreter=dap', '--eval-command', 'set print pretty on' }
-}
-
 dap.adapters.cppdbg = {
 	id = 'cppdbg',
 	type = 'executable',
 	command = os.getenv("HOME") .. '/.local/share/debug_adapters/extension/debugAdapters/bin/OpenDebugAD7',
 }
 
-dap.adapters.lldb = {
-	type = 'executable',
-	command = '/usr/bin/lldb-dap',
-	name = 'lldb',
-	args = {},
-}
-
-
 dap.configurations.c = {
 	{
-		name = 'Launch',
-		type = 'lldb',
+		name = 'generic - Launch',
+		type = 'cppdbg',
 		request = 'launch',
 		program = function()
 			return vim.fn.input('Path to executable:', vim.fn.getcwd() .. '/', 'file')
 		end,
-		runInTerminal = true,
+		externalConsole = true,
 	},
 	{
-		name = 'Launch nvim headless',
-		type = 'lldb',
-		request = 'launch',
-		program = function()
-			return vim.fn.getcwd() .. '/build/bin/nvim'
-		end,
-		args = { '--headless', '--listen', 'localhost:12345' },
-	},
-	{
-		name = 'Attach to gdb-server',
+		name = 'generic - Attach to gdb-server',
 		type = 'cppdbg',
 		request = 'launch',
 		program = function()
 			return vim.fn.input('Path to executable:', vim.fn.getcwd() .. '/', 'file')
 		end,
 		miDebuggerServerAddress = 'localhost:7777',
-		cwd = '.'
+		cwd = vim.fn.getcwd()
+	},
+	setmetatable({
+			name = 'nvim - Launch Full, Debug Server',
+			type = 'cppdbg',
+			request = 'launch',
+			program = function()
+				return vim.fn.getcwd() .. '/build/bin/nvim'
+			end,
+			externalConsole = true,
+			cwd = '.'
+		},
+		{
+			__call = function(config)
+				local key = 'gf_nvim_launch_full_debug_server'
+				dap.listeners.after.initialize[key] = function(session)
+					dap.listeners.after.initialize[key] = nil
+					session.on_close[key] = function()
+						for _, handler in pairs(dap.listeners.after) do
+							handler[key] = nil
+						end
+					end
+				end
+
+				dap.listeners.after.event_process[key] = function(_, body)
+					dap.listeners.after.event_process[key] = nil
+
+					local ppid = body.systemProcessId
+
+					vim.wait(1000, function()
+						return tonumber(vim.fn.system("ps -o pid= --ppid " .. tostring(ppid))) ~= nil
+					end)
+					local pid = tonumber(vim.fn.system("ps -o pid= --ppid " .. tostring(ppid)))
+					assert(pid ~= nil, "child process ID should not be nil")
+
+					if pid then
+						dap.run({
+							name = "Neovim embedded",
+							type = "cppdbg",
+							request = "attach",
+							processId = pid,
+							program = vim.fn.getcwd() .. "/build/bin/nvim",
+							cwd = vim.fn.getcwd(),
+							externalConsole = true,
+						})
+					end
+				end
+				return config
+			end
+		}),
+	{
+		name = 'nvim - Launch Headless',
+		type = 'cppdbg',
+		request = 'launch',
+		program = function()
+			return vim.fn.getcwd() .. '/build/bin/nvim'
+		end,
+		args = { '--headless', '--listen', 'localhost:7777' },
+		externalConsole = true,
+		cwd = vim.fn.getcwd()
 	},
 	{
 		name = 'nvim - Attach Tests',
@@ -62,7 +98,7 @@ dap.configurations.c = {
 			return vim.fn.getcwd() .. '/build/bin/nvim'
 		end,
 		miDebuggerServerAddress = 'localhost:7777',
-		cwd = '.'
+		cwd = vim.fn.getcwd()
 	},
 }
 
