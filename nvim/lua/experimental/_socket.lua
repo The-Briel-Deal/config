627c2686bfc0afbc5e1d010a9b274c7733223748
Gabriel Ford <gabe@fordltc.net>
2025-06-13 17:27:59 -0400

    format _socket.lua

--- nvim/lua/experimental/_socket.lua
+++ nvim/lua/experimental/_socket.lua
@@ -1,86 +1,86 @@
 local socket = require 'socket'
 
-local function receive (connection)
+local function receive(connection)
   return connection:receive(128)
 end
 
-
-local function download (host, file)
+local function download(host, file)
   local c = assert(socket.connect(host, 80))
-  local count = 0    -- counts number of bytes read
-  c:send("GET " .. file .. " HTTP/1.0\r\n\r\n")
-	local str = ''
+  local count = 0 -- counts number of bytes read
+  c:send('GET ' .. file .. ' HTTP/1.0\r\n\r\n')
+  local str = ''
   while true do
     local s, status, partial = receive(c)
-    if status == "closed" then
-			str = str .. partial
-			break
-		end
+    if status == 'closed' then
+      str = str .. partial
+      break
+    end
     if s then
       count = count + string.len(s)
-			str = str .. s
+      str = str .. s
     end
   end
   c:close()
-	return str
+  return str
 end
 local function downloadn(host, file, n)
-	for i=1,n do
-		download(host, file)
-	end
+  for i = 1, n do
+    download(host, file)
+  end
 end
 
-local function async_download (host, file)
+local function async_download(host, file)
   local c = assert(socket.connect(host, 80))
   c:timeout(0)
-  local count = 0    -- counts number of bytes read
-  c:send("GET " .. file .. " HTTP/1.0\r\n\r\n")
-	local str = ''
+  local count = 0 -- counts number of bytes read
+  c:send('GET ' .. file .. ' HTTP/1.0\r\n\r\n')
+  local str = ''
   while true do
     local s, status, partial = receive(c)
-		if status == "timeout" then
-		  coroutine.yield(nil)
-		elseif status == "closed" then
-			str = str .. partial
+    if status == 'timeout' then
+      coroutine.yield(nil)
+    elseif status == 'closed' then
+      str = str .. partial
       c:close()
-			coroutine.yield(str)
-			break
-		end
+      coroutine.yield(str)
+      break
+    end
     if s then
       count = count + string.len(s)
-			str = str .. s
+      str = str .. s
     end
   end
 end
 local function async_downloadn(host, file, n)
-	local threads = {}
-	for i=1,n do
-		table.insert(threads, coroutine.create(function()
-	  	async_download(host, file)
-	  end))
-	end
-	while #threads > 0 do
-		for i=1,#threads do
+  local threads = {}
+  for i = 1, n do
+    table.insert(
+      threads,
+      coroutine.create(function()
+        async_download(host, file)
+      end)
+    )
+  end
+  while #threads > 0 do
+    for i = 1, #threads do
       local status, str = coroutine.resume(threads[i])
-			if str ~= nil then
+      if str ~= nil then
         table.remove(threads, i)
-				-- print(#str)
-				break
-			end
-		end
-	end
+        -- print(#str)
+        break
+      end
+    end
+  end
 end
 
-local host = "localhost"
-local file = "/"
-
+local host = 'localhost'
+local file = '/'
 
 local start_time = socket.gettime()
 downloadn(host, file, 100)
 local end_time = socket.gettime()
 
 print('sync', (end_time - start_time))
-
 
 start_time = socket.gettime()
 async_downloadn(host, file, 100)
