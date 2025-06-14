b4debef715232317fa54d64bd488a3d1305deb2b
Gabriel Ford <gabe@fordltc.net>
2025-06-13 21:20:42 -0400

    speed up socket

--- nvim/lua/experimental/_socket.lua
+++ nvim/lua/experimental/_socket.lua
@@ -1,4 +1,5 @@
 local socket = require 'socket'
+local inspect = require 'inspect'
 
 local function receive(connection)
   return connection:receive(128)
@@ -29,8 +30,7 @@ local function downloadn(host, file, n)
   end
 end
 
-local function async_download(host, file)
-  local c = assert(socket.connect(host, 80))
+local function async_download(c, host, file)
   c:timeout(0)
   local count = 0 -- counts number of bytes read
   c:send('GET ' .. file .. ' HTTP/1.0\r\n\r\n')
@@ -54,36 +54,53 @@ end
 local function async_downloadn(host, file, n)
   local threads = {}
   for i = 1, n do
-    table.insert(
-      threads,
-      coroutine.create(function()
-        async_download(host, file)
-      end)
-    )
+    local c = assert(socket.connect(host, 80))
+    table.insert(threads, {
+			closed = false,
+      cor = coroutine.create(function()
+        async_download(c, host, file)
+      end),
+      con = c,
+      getfd = function(t)
+        return t.con:getfd()
+      end,
+      dirty = function(t)
+        return t.con:dirty()
+      end,
+    })
   end
-  while #threads > 0 do
-    for i = 1, #threads do
-      local status, str = coroutine.resume(threads[i])
+  local check_threads = function(to_check)
+    for i = 1, #to_check do
+      local cor_status, str = coroutine.resume(to_check[i].cor)
       if str ~= nil then
-        table.remove(threads, i)
-        -- print(#str)
-        break
+				to_check[i].closed = true
       end
     end
   end
+  while #threads > 0 do
+    local to_read, to_write, select_status = socket.select(threads, threads, 0)
+		print(inspect(to_read, to_write))
+    check_threads(to_read)
+    check_threads(to_write)
+		for i, thread in ipairs(threads) do
+			if thread.closed then
+				table.remove(threads, i)
+			end
+		end
+  end
 end
 
-local host = 'localhost'
-local file = '/'
+local host = 'lunarmodules.github.io'
+local file = '/luasocket/socket.html'
 
 local start_time = socket.gettime()
-downloadn(host, file, 100)
+async_downloadn(host, file, 20)
 local end_time = socket.gettime()
 
-print('sync', (end_time - start_time))
+print('async', (end_time - start_time))
 
 start_time = socket.gettime()
-async_downloadn(host, file, 100)
+downloadn(host, file, 20)
 end_time = socket.gettime()
 
-print('async', (end_time - start_time))
+print('sync', (end_time - start_time))
