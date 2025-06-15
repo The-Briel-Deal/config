05f25699e0fe0242e8de105c5af8595736d513b2
Gabriel Ford <gabe@fordltc.net>
2025-06-15 15:30:34 -0400

    add floating window test

--- nvim/test/float_spec.lua
+++ nvim/test/float_spec.lua
@@ -6,21 +6,37 @@ local feed = n.feed
 local exec_lua = n.exec_lua
 
 describe('Floating window', function()
-  it('appears', function()
+	---@type test.functional.ui.screen
+	local screen
+
+	before_each(function ()
     clear()
-    local screen = Screen.new(20, 10)
-		exec_lua(function()
-			print('hello world')
-		end)
+    screen = Screen.new(20, 10)
+	end)
+
+  it('appears', function()
+    exec_lua(function()
+      local float = require('float')
+      local fw = float.New { title = 'foo', width = 10, height = 5, body = 'foo\nbar\nbaz' }
+    end)
     screen:add_extra_attr_ids({
       [101] = { foreground = Screen.colors.NvimLightGray4 },
       [102] = { foreground = Screen.colors.NvimDarkGreen },
+      [103] = { background = Screen.colors.NvimLightGrey1 },
+      [104] = { foreground = Screen.colors.NvimDarkGrey2, bold = true },
     })
 
     screen:expect([[
-      hello world^         |
-      {101:~                   }|*8
-      {102:-- INSERT --}        |
+      ^                    |
+      {101:~}{103:╭}{104:foo}{103:───────╮}{101:       }|
+      {101:~}{103:│foo       │}{101:       }|
+      {101:~}{103:│          │}{101:       }|
+      {101:~}{103:│bar       │}{101:       }|
+      {101:~}{103:│          │}{101:       }|
+      {101:~}{103:│baz       │}{101:       }|
+      {101:~}{103:╰──────────╯}{101:       }|
+      {101:~                   }|
+                          |
     ]])
   end)
 end)
