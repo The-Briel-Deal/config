810e0a4796445073a8b35c3d7ff2617355d4c665
Gabriel Ford <gabe@fordltc.net>
2025-06-15 14:15:13 -0400

    add screen test util and use it in example test

--- nvim/test/float_spec.lua
+++ nvim/test/float_spec.lua
@@ -1,8 +1,23 @@
-local t = require 'util.testutil'
-local n = require 'util.testnvim'
+local t = require('util.testutil')
+local n = require('util.testnvim')()
+local Screen = require('util.screen')
+local clear = n.clear
+local feed = n.feed
 
 describe('Testing busted', function()
   it('should work', function()
-    assert.truthy('yes pwease')
+    clear()
+    local screen = Screen.new(20, 10)
+    feed('ihello world')
+    screen:add_extra_attr_ids({
+      [101] = { foreground = Screen.colors.NvimLightGray4 },
+      [102] = { foreground = Screen.colors.NvimDarkGreen },
+    })
+
+    screen:expect([[
+      hello world^         |
+      {101:~                   }|*8
+      {102:-- INSERT --}        |
+    ]])
   end)
 end)
