9cdbbf58e4d943b0d66fa759936105289a24d176
Gabriel Ford <gabe@fordltc.net>
2025-07-24 15:53:41 +0000

    enable typescript lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,8 @@ vim.pack.add{
   { src = 'https://github.com/neovim/nvim-lspconfig' },
 }
 
+vim.lsp.enable('ts_ls')
+
 require('plugin').setup {}
 require('config').setup {}
 require('keymap').setup {}
