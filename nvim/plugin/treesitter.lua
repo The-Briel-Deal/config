4a027e3650dcc9b100481a38a2ee143a9334a65a
Gabriel Ford <gabe@fordltc.net>
2025-08-14 14:59:37 +0000

    enable sync installs treesitter

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -2,7 +2,7 @@ require 'nvim-treesitter.configs'.setup {
   modules = {},
   ensure_installed = {},
   ignore_install = {},
-  sync_install = false,
+  sync_install = true,
   auto_install = true,
   highlight = {
     enable = true,
