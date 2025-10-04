819118d32384ca96bc58d0ca595078acb5ecea45
Gabriel Ford <gabe@fordltc.net>
2025-10-04 16:39:42 -0400

    Remove whitespace

--- fish/functions/gitup.fish
+++ fish/functions/gitup.fish
@@ -1,4 +1,3 @@
 function gitup --wraps='git checkout main && git pull && git checkout - && git rebase main' --description 'alias gitup=git checkout main && git pull && git checkout - && git rebase main'
   git checkout main && git pull && git checkout - && git rebase main $argv
-        
 end
