f18da704483bb9273c62c29b1b0859b70fa690e6
Gabriel Ford <gabe@fordltc.net>
2025-10-31 18:53:59 +0000

    use recommended luals settings

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -1,29 +1,45 @@
-vim.lsp.config('luals', {
-  name = 'luals',
-  cmd = { 'lua-language-server' },
-  filetypes = { 'lua' },
-  root_dir = vim.fs.dirname(
-    vim.fs.find(
-      { '.luarc.json', 'init.lua', '.gitignore' },
-      { upward = true, path = vim.api.nvim_buf_get_name(0) }
-    )[1]
-  ),
-  settings = {
-    Lua = {
-      hint = { enable = true },
-      workspace = {
-        library = {
-          vim.env.VIMRUNTIME,
-          '${3rd}/busted/library',
-          '${3rd}/luassert/library',
-          '${3rd}/luv/library',
+vim.lsp.config('lua_ls', {
+  on_init = function(client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if
+        path ~= vim.fn.stdpath('config')
+        and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+      then
+        return
+      end
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most
+        -- likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Tell the language server how to find Lua modules same way as Neovim
+        -- (see `:h lua-module-load`)
+        path = {
+          'lua/?.lua',
+          'lua/?/init.lua',
         },
-        checkThirdParty = 'Enable',
       },
-    },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        -- library = {
+        --   vim.env.VIMRUNTIME,
+        -- },
+        -- Or pull in all of 'runtimepath'.
+        -- NOTE: this is a lot slower and will cause issues when working on
+        -- your own configuration.
+        -- See https://github.com/neovim/nvim-lspconfig/issues/3189
+        library = vim.api.nvim_get_runtime_file('', true),
+      },
+    })
+  end,
+  settings = {
+    Lua = {},
   },
 })
-
 vim.lsp.config('zls', {
   settings = {
     zls = {
@@ -49,7 +65,7 @@ vim.lsp.enable({
   'pyright',
   'clangd', --[['glsl_analyzer', --]]
   'gopls',
-  'luals',
+  'lua_ls',
   'zls',
 })
 
