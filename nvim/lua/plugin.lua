6fb299356ef8d50ba38afd7e3b691178554fcfcd
Gabriel Ford <gabe@fordltc.net>
2026-01-21 19:58:01 +0000

    Combine vim.pack.add() calls

--- nvim/lua/plugin.lua
+++ nvim/lua/plugin.lua
@@ -3,55 +3,39 @@ local M = {}
 M.setup = function(opts)
   vim.pack.add({
     { src = 'https://github.com/folke/tokyonight.nvim.git' },
-  })
-  vim.pack.add({
+
     { src = 'https://github.com/neovim/nvim-lspconfig' },
-  })
-  vim.pack.add({
+
     { src = 'https://github.com/linux-cultist/venv-selector.nvim.git' },
-  })
-  require('venv-selector').setup {}
-  vim.pack.add({
     { src = 'https://github.com/tpope/vim-fugitive.git' },
-  })
-  vim.pack.add({
+
     { src = 'https://github.com/tpope/vim-dadbod.git' },
     { src = 'https://github.com/kristijanhusak/vim-dadbod-ui.git' },
     { src = 'https://github.com/kristijanhusak/vim-dadbod-completion.git' },
-  })
-  vim.pack.add({
+
     { src = 'https://github.com/stevearc/oil.nvim.git' },
-  })
-  vim.pack.add({
+
     { src = 'https://github.com/nvim-treesitter/nvim-treesitter.git' },
-  })
-  vim.pack.add({
+
     { src = 'https://github.com/nvim-telescope/telescope.nvim.git' },
-  })
-  vim.pack.add({
+
     { src = 'https://github.com/nvim-lua/plenary.nvim.git' },
-  })
-  vim.pack.add({
+
     { src = 'https://github.com/Saghen/blink.cmp.git', version = 'v1.6.0' },
-  })
-  vim.pack.add({
+
     { src = 'https://github.com/mfussenegger/nvim-dap.git' },
-  })
-  vim.pack.add({
+
     { src = 'https://github.com/theHamsta/nvim-dap-virtual-text.git' },
-  })
-  vim.pack.add({
+
     { src = 'https://github.com/igorlfs/nvim-dap-view.git' },
-  })
-  vim.pack.add({
+
     { src = 'https://github.com/stevearc/conform.nvim.git' },
-  })
-  vim.pack.add({
+
     { src = 'https://github.com/MeanderingProgrammer/render-markdown.nvim.git' },
+
+    { src = 'https://github.com/ramilito/kubectl.nvim.git', version = 'v2.24.0' },
   })
-  vim.pack.add({
-    { src = 'https://github.com/ramilito/kubectl.nvim.git', version = "v2.24.0" },
-  })
+  require('venv-selector').setup {}
   require('tokyonight').setup {
     transparent = true,
   }
