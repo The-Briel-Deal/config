7a76b7ce1bdcb1e49a5929273a9bb4d8547d3227
Gabriel Ford <gabe@fordltc.net>
2026-03-28 15:09:11 -0400

    automatically enable treesitter highlighting for supported files

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -8,3 +8,17 @@ nv_ts.install({
   'markdown',
   'markdown_inline',
 })
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'c', 'cpp', 'python', 'lua', 'markdown' },
+  callback = function()
+    -- syntax highlighting, provided by Neovim
+    vim.treesitter.start()
+    -- folds, provided by Neovim
+    vim.wo.foldlevel = 4
+    vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    vim.wo.foldmethod = 'expr'
+    -- indentation, provided by nvim-treesitter
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  end,
+})
