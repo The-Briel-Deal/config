cc20d55842dd5f06431f1d794c57df65028e8791
Gabriel Ford <gabe@fordltc.net>
2026-03-29 10:22:17 -0400

    create a termdebug wrapper command which sets up keybinds

---
+++ nvim/plugin/term_debug.lua
@@ -0,0 +1,23 @@
+vim.api.nvim_create_user_command('GFTermDebug', function(args)
+  vim.cmd.packadd('termdebug')
+  vim.api.nvim_set_keymap(
+    'n',
+    '<A-s>',
+    ':Step<CR>',
+    { desc = 'termdebug: Move onto the next line, step into calls.' }
+  )
+  vim.api.nvim_set_keymap(
+    'n',
+    '<A-n>',
+    ':Over<CR>',
+    { desc = 'termdebug: Move onto the next line, skip over calls.' }
+  )
+  vim.api.nvim_set_keymap('n', '<A-b>', ':Break<CR>', { desc = 'termdebug: Set breakpoint.' })
+  vim.api.nvim_set_keymap('n', '<A-B>', ':Clear<CR>', { desc = 'termdebug: Clear breakpoint.' })
+  vim.api.nvim_set_keymap(
+    'n',
+    '<A-c>',
+    ':Continue<CR>',
+    { desc = 'termdebug: Continue execution until next breakpoint.' }
+  )
+end, { desc = "Wrapper around vim's built in termdebug package." })
