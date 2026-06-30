2d6ace64b6087c66946d9fa2993ec7a6885ab68f
Gabriel Ford <gabe@fordltc.net>
2026-06-30 15:52:24 -0400

    reorder cmake-test alias

--- fish/functions/cmake-test.fish
+++ fish/functions/cmake-test.fish
@@ -1,3 +1,3 @@
-function cmake-test --wraps='ctest --build-and-test $(pwd) $(realpath build) --build-generator=Ninja --build-noclean --test-command tests' --description 'alias cmake-test ctest --build-and-test $(pwd) $(realpath build) --build-generator=Ninja --build-noclean --test-command tests'
-    ctest --build-and-test $(pwd) $(realpath build) --build-generator=Ninja --build-noclean --test-command tests $argv
+function cmake-test --wraps='ctest --build-and-test $(pwd) $(realpath build) --build-generator=Ninja --build-noclean --build-target test_exe --test-command test_exe' --description 'alias cmake-test ctest --build-and-test $(pwd) $(realpath build) --build-generator=Ninja --build-noclean --build-target test_exe --test-command test_exe'
+    ctest --build-and-test $(pwd) $(realpath build) --build-generator=Ninja --build-noclean --build-target test_exe --test-command test_exe $argv
 end
