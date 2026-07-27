9569e7519649dc770dc39092a9eb182cc52f6074
Gabriel Ford <gabe@fordltc.net>
2026-07-27 17:18:15 -0400

    disable folding by default

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -44,6 +44,7 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.wo.foldlevel = 4
     vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
     vim.wo.foldmethod = 'expr'
+    vim.wo.foldenable = false -- disable folding by default
     -- indentation, provided by nvim-treesitter
     vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
   end,
